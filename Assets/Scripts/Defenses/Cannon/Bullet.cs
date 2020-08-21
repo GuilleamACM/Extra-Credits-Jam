ede267ce33ee76c2587ef4f5c5196fa9061decf9
Matheus <mbmt@cin.ufpe.br>
2020-08-21 19:27:22 -0300

    Creating Bullet and CannonHead

---
+++ Assets/Scripts/Defenses/Cannon/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float Speed = 5.0f;
+    public Vector3 Direction;
+    public int Damage = 10;
+
+    private void Start()
+    {
+        this.Direction = this.Direction.normalized;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position += Direction * Time.deltaTime * Speed;
+    }
+}
---
+++ Assets/Scripts/Defenses/Cannon/CannonHead.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonHead : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+
+    private void Start()
+    {
+        Fire();
+    }
+
+    public void Fire()
+    {
+        var bullet = Instantiate(BulletPrefab,transform);
+        bullet.transform.position = Vector3.zero;
+        bullet.GetComponent<Bullet>().Direction = this.transform.forward;
+    }
+}
