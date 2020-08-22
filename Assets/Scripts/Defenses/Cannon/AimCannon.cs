5c5f053f5440bc7d71318f3d26c74fce81e91918
Matheus <mbmt@cin.ufpe.br>
2020-08-21 22:32:45 -0300

    Creating Spiral and Aim cannon

---
+++ Assets/Scripts/Defenses/Cannon/AimCannon.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimCannon : MonoBehaviour
+{
+    public CannonHead cannon;
+    public float TimeAiming;
+    public GameObject EnemysParent;
+
+
+    bool targetLocked = false;
+    float currentTime = 0;
+    [SerializeField]
+    Transform target;
+    float distance;
+    //private List<Enemy> enemys;
+
+    private void Start()
+    {
+        //this.enemys = GameObject.FindObjectOfType<EnemysManager>().GetEnemys();
+    }
+
+    private void Update()
+    {
+        if (targetLocked && target)
+        {
+            this.transform.LookAt(target,transform.forward);//verificar isso
+            this.currentTime += Time.deltaTime;
+            if (this.currentTime >= TimeAiming) 
+            {
+                Fire();
+                ResetTimer();
+                this.targetLocked = false;
+                this.target = null;
+            }
+        }
+        else 
+        {
+            TryLockTarget();
+        }
+    }
+
+    void Fire() 
+    {
+        cannon.Fire();
+    }
+
+    void ResetTimer() 
+    {
+        this.currentTime = 0;
+    }
+
+    void TryLockTarget() 
+    {
+        //target = null;
+        //foreach (Enemy enemy in enemys) 
+        //{
+        //    float distance = Vector3.Distance(target.transform.position, enemy.transform.position);
+        //    if (!target || distance < this.distance) 
+        //    {
+        //        this.target = enemy.transform;
+        //        this.distance = distance;
+        //    }
+        //}
+    }
+}
---
+++ Assets/Scripts/Defenses/Cannon/Spin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 2f;
+    private void Update()
+    {
+        transform.Rotate(transform.forward, Time.deltaTime * speed);
+    }
+}
