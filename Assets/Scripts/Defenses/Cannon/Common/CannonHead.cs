f0a0bc21c47c924643a188d720b5b4eec5d59061
GuilleamACM <gacm@cin.ufpe.br>
2020-08-23 17:33:59 -0300

    Towers and Bullets Shaders, Materials, Sprites and Prefabs

--- Assets/Scripts/Defenses/Cannon/Common/CannonHead.cs
+++ Assets/Scripts/Defenses/Cannon/Common/CannonHead.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CannonHead : MonoBehaviour
@@ -16,14 +17,13 @@ public class CannonHead : MonoBehaviour
     {
         var bullet = Instantiate(BulletPrefab, bulletHolder);
         bullet.transform.position = transform.position;
-        bullet.transform.localScale = Vector3.one;
+        bullet.transform.eulerAngles = transform.eulerAngles + new Vector3(0, 0, 90);
         bullet.GetComponent<Bullet>().Direction = this.transform.right;
     }
     public void Fire(Vector3 dir)
     {
         var bullet = Instantiate(BulletPrefab, bulletHolder);
         bullet.transform.position = transform.position;
-        bullet.transform.localScale = Vector3.one;
         bullet.GetComponent<Bullet>().Direction = dir;
     }
 }
