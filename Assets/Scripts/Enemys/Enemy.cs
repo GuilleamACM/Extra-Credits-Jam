eeb3c9847c2e4d01e1f6a985948e1ddd44f1c840
Matheus <mbmt@cin.ufpe.br>
2020-08-23 15:23:44 -0300

    Creating healler

--- Assets/Scripts/Enemys/Enemy.cs
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -15,11 +15,48 @@ public class Enemy : MonoBehaviour
     private int health;
 
     private bool isDead;
+    public EnemyType _type = EnemyType.Normal;
+
+    public float HealRange = 5f;
+    public int HealAmount = 20;
+    public float HealCooldown = 5f;
+
+    public enum EnemyType 
+    {
+        Normal,
+        Healer
+    }
 
     private void Start()
     {
         MovementSpeed = defaultMovementSpeed;
         health = maxHealth;
+        if (this._type == EnemyType.Healer) 
+        {
+            StartCoroutine(HealCoroutine());
+        }
+    }
+
+    IEnumerator HealCoroutine() 
+    {
+        yield return new WaitForSeconds(HealCooldown);
+        HealEnemysAround();
+    }
+
+    void HealEnemysAround() 
+    {
+        foreach (Transform t in WaveSpawner.Instance.GetEnemiesTransform())
+        {
+            if (Vector3.Distance(this.transform.position, t.position) <= HealRange)
+            {
+                t.GetComponent<Enemy>().Heal(HealAmount);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        this.StopAllCoroutines();
     }
 
     public void TakeDamage(int amount)
