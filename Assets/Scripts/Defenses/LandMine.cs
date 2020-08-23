08589546966d448e6e915640376a7b5b53e121a9
Matheus <mbmt@cin.ufpe.br>
2020-08-23 10:40:04 -0300

    creating landmine

---
+++ Assets/Scripts/Defenses/LandMine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandMine : MonoBehaviour
+{
+    public static string EnemyTag = "Enemy";
+    public int Damage = 0;
+    [Range(0f, 1f)]
+    public float slow = .5f;
+    public float time = 2f;
+    private List<Transform> enemies;
+
+    private void Start()
+    {
+        this.enemies = WaveSpawner.Instance.GetEnemiesTransform();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HitCheck(collision);
+    }
+
+    void HitCheck(Collider2D collision)
+    {
+        if (collision.CompareTag(EnemyTag))
+        {
+            collision.GetComponent<Enemy>().TakeDamage(this.Damage);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
