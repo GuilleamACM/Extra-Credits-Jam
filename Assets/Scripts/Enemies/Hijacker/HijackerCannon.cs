186a9ea374f4a620fb6988da7e4340fb0a93668f
Matheus <mbmt@cin.ufpe.br>
2020-08-24 10:06:26 -0300

    Adding hijacker fire mechanic

---
+++ Assets/Scripts/Enemies/Hijacker/HijackerCannon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HijackerCannon : MonoBehaviour
+{
+    public GameObject HijackerBullet;
+    public float timeBetweenShots = 1f;
+    float currentTime;
+
+    public Vector3[] fireDirections;
+    public int directionIndex;
+
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= timeBetweenShots) 
+        {
+            Fire();
+            ResetTimer();
+        }
+    }
+
+    void ResetTimer() 
+    {
+        currentTime = 0f;
+    }
+
+    void Fire() 
+    {
+        UpdateDirection();
+        Instantiate(HijackerBullet);
+        HijackerBullet.transform.position = this.transform.position;
+        HijackerBullet.GetComponent<HijackerBullet>().direction = fireDirections[directionIndex];
+    }
+    void UpdateDirection() 
+    {
+        directionIndex = (directionIndex + 1) % fireDirections.Length;
+    }
+
+}
