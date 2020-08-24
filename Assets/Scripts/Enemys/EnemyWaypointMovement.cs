b9817c679c22def99b268fa8e5ae4379a0e5d6a4
IvanNSBS <insbs@cin.ufpe.br>
2020-08-23 21:48:34 -0300

    Finishing enemy pathfinding and preparing hijacker rework

--- Assets/Scripts/Enemys/EnemyWaypointMovement.cs
+++ Assets/Scripts/Enemys/EnemyWaypointMovement.cs
@@ -5,8 +5,6 @@ using TinyGecko.Pathfinding2D;
 [RequireComponent(typeof(Enemy))]
 public class EnemyWaypointMovement : MonoBehaviour
 {
-    public WaypointType type = WaypointType.Normal;
-    private Pathfinder pf;
     private Queue<GridCel> path;
     private Transform targetTower;
     private Vector3 target;
@@ -16,12 +14,6 @@ public class EnemyWaypointMovement : MonoBehaviour
 
     private Enemy enemy;
 
-    public enum WaypointType 
-    {
-        Normal,
-        Hijacker
-    }
-
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -30,41 +22,13 @@ public class EnemyWaypointMovement : MonoBehaviour
     private void Start()
     {
         enemy = GetComponent<Enemy>();
-        if (this.type == WaypointType.Normal)
-        {
-            Structure core = Waypoints.CoreSructure;
-            path = WorldGrid.Instance.Pathfinder.FindPath(transform.position, core);
-        }
-        else 
-        {
-            SetPathHijacker();
-        }
-    }
-
-    void SetPathHijacker() 
-    {
-        this.pf = new Pathfinder(WorldGrid.Instance);
-        var sm = StructureManager.Instance;
-        if (sm.PlacedStructures.Count > 0)
-        {
-            targetTower = sm.PlacedStructures[Random.Range(0, sm.PlacedStructures.Count)].transform;
-            path = pf.FindPath(transform.position, targetTower.GetComponent<Structure>());
-            target = path.Dequeue().worldPos;
-            noTower = false;
-        }
-        else 
-        {
-            noTower = true;
-            target = Waypoints.waypoints[currentWaypointIndex].position;
-        }
+        Structure core = Waypoints.CoreSructure;
+        path = WorldGrid.Instance.Pathfinder.FindPath(transform.position, core);
+        target = path.Dequeue().worldPos;
     }
 
     private void Update()
     {
-        if (type == WaypointType.Hijacker && !targetTower) 
-        {
-            SetPathHijacker();
-        }
         Vector3 waypointDirection = (target - transform.position).normalized * enemy.MovementSpeed * Time.deltaTime;
         float distance = Vector3.Distance(transform.position, target);
         if (distance <= Vector3.Distance(transform.position + waypointDirection,target)) 
@@ -79,39 +43,20 @@ public class EnemyWaypointMovement : MonoBehaviour
 
     private void GetNextWaypoint()
     {
-        if (this.type == WaypointType.Normal || noTower)
+        if (path.Count == 0)
         {
-            if (path.Count == 0)
-            {
-                ReachObjective();
-                return;
-            }
+            ReachObjective();
+            return;
+        }
 
-            target = path.Dequeue().worldPos;
-        }
-        else 
-        {
-            if (path.Count == 0) 
-            {
-                ReachObjective();
-                return;
-            }
-            target = path.Dequeue().worldPos;
-        }
+        target = path.Dequeue().worldPos;
     }
 
     private void ReachObjective()
     {
         //Decrease the Player RAM or CPU
-        if (type == WaypointType.Normal || noTower)
-        {
-            PlayerStatus.Instance.UsedMemory += enemy.memoryUsage;
-            WaveSpawner.Instance.RemoveEnemy(enemy);
-            Destroy(gameObject);
-        }
-        else 
-        {
-            enemy.HijackTower(targetTower.GetComponent<Tower>());
-        }
+        PlayerStatus.Instance.UsedMemory += enemy.memoryUsage;
+        WaveSpawner.Instance.RemoveEnemy(enemy);
+        Destroy(gameObject);
     }
 }
